4b777f8e9afa99b537427da58a455f5bd7266fb1
Iñigo Romero <45758515+InigoRomero@users.noreply.github.com>
2020-11-19 20:12:27 +0100

    ex00 without peon class

---
+++ ex00/main.cpp
@@ -0,0 +1,16 @@
+#include "Sorcerer.hpp"
+
+int main()
+{
+    Sorcerer robert("Robert", "the Magnificent");
+
+    Victim jim("Jimmy");
+    Peon joe("Joe");
+
+    std::cout << robert << jim << joe;
+
+    robert.polymorph(jim);
+    robert.polymorph(joe);
+    
+    return 0;
+}
